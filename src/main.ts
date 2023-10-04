dcf39d1332c5baead0e3f8d82dd5b8bcbf9e662f
mg-yamanedaiki <d.yamane@mountain-gorilla.co.jp>
2023-10-04 10:17:36 +0900

    fix hot reload

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Nestjs example')
     .setDescription('API description')
-    .setVersion('1.0')
+    .setVersion(process.env.ENV || 'dev')
     .addTag('nestjs')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(3001);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();

b414184e4b4794e5dae441a170978ab293da0a8e
daikiyamane <daiki04752@gmail.com>
2024-08-08 03:36:17 +0900

    seed修正

--- src/db/seeds/database.ts
+++ src/db/seeds/database.ts
@@ -2,7 +2,7 @@ import { type MySql2Database, drizzle } from "drizzle-orm/mysql2";
 
 import * as mysql2 from "mysql2/promise";
 import { MYSQL_CREDENTIALS } from "src/config";
-import * as schema from "./schema";
+import * as schema from "../schema";
 import * as user from "./user";
 const main = async () => {
 	const connection = await mysql2.createConnection(MYSQL_CREDENTIALS);
--- src/db/seeds/user.ts
+++ src/db/seeds/user.ts
@@ -1,5 +1,5 @@
 import type { MySql2Database } from "drizzle-orm/mysql2";
-import * as schema from "./schema";
+import * as schema from "../schema";
 
 export async function run(db: MySql2Database<typeof schema>) {
 	await db.insert(schema.users).values({
